d1b6454c07b036b4b36bcc46e3488c62cb42d742
Gabriel Santos <gabrieljbs@gmail.com>
2024-05-27 14:14:40 -0300

    Routing service

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+declare module 'leaflet' {
+  namespace Routing {
+    function control(options: any): any;
+  }
+  namespace Control {
+    namespace Geocoder {
+      function nominatim(options?: any): any;
+    }
+  }
+}
